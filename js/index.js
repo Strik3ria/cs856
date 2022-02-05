d00182641c295808e16768657b195a0d21fac0c7
Strik3ria <robert_coones@zohomail.com>
2022-02-05 10:35:22 -0600

    added content to webpage

--- js/index.js
+++ js/index.js
@@ -2,3 +2,11 @@ const setActive = (activePage) => {
   let navEl = document.getElementById(activePage);
   navEl.classList.add("active");
 };
+
+const getNameParameter = (url) => {
+  return url.split("?")[1].split("=")[1];
+};
+
+const properCaseWithSpace = (str) => {
+  return str.match(/[A-Z][a-z]+/g).join(' ');
+};
